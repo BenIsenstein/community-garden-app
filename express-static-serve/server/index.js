4cfdf71eb237a2a53ff829e04b6a4e7fe0aaa4ed
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-05-05 14:46:20 -0700

    express v.0

---
+++ express-static-serve/server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express(); // create express app
+
+app.use(express.static("public")); // add middleware
+
+// start express server on port 5000
+app.listen(5000, () => {
+    console.log("server started on port 5000");
+});
+
