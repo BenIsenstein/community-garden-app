cae8e0f8450b83146fb0c00d0fe631ce4bcdb425
Ben Isenstein <ben.isenstein@gmail.com>
2021-05-18 14:07:03 -0600

    Got <LandingPage /> to work. It displays an <h1> with the title of the garden.

--- client/src/components/IndividualGardens/LandingPage.js
+++ client/src/components/IndividualGardens/LandingPage.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-export default function LandingPage({name})  {
-    const [gardenData, setGardenData] = useState ([])
+export default function LandingPage({ name }) {
+  const [gardenData, setGardenData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => { 
-    
-            let response=await fetch(`/api/get-all-gardens/individual-garden?name=${name}`)
-            let resObject=await response.json() 
-            setGardenData(resObject.garden)
-            console.log(gardenData)
-        } 
-        fetchData()
-    },[]) 
-    return null
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch(
+        `/api/get-all-gardens/individual-garden?name=${name}`
+      );
+      let resObject = await response.json();
+      let gardenObject = resObject.garden;
+      setGardenData(gardenObject);
+    };
+    fetchData();
+  }, []);
+
+  return <h1>{gardenData ? "Loading..." : gardenData.name}</h1>;
 }
-
