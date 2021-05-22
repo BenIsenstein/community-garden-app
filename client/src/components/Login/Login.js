d2620256fe0f5ef1cdea816f015b39b43f61add6
Christy <57055910+christyc01@users.noreply.github.com>
2021-05-21 23:01:53 -0600

    Remove unused code; improve form submission

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -4,32 +4,8 @@ import { useForm } from "react-hook-form"
 import "../Signup/Signup.css"
 
 const Login = () => {
-  const {
-    // register,
-    // formState: { errors },
-    handleSubmit
-    // watch
-  } = useForm({})
-  // const password = useRef({})
-  // const confirmPassword = useRef({})
-  // password.current = watch("password", "")
-  // confirmPassword.current = watch("confirmPassword", "")
-
-  async function onSubmit(data) {
-    let fetchUrl = "/api/login"
-    let fetchOptions = {
-      method: "post",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(data)
-    }
-    let response = await fetch(fetchUrl, fetchOptions)
-    console.log("response: ", response)
-    // let resObject = await response.json()
-    console.log("login submit worked!!")
-  }
-
   return (
-    <form className="signupForm" onSubmit={handleSubmit(onSubmit)}>
+    <form className="signupForm" action="/api/login" method="POST">
       <div className="container">
         <h1>Log In!</h1>
         <div className="form-control">
@@ -41,19 +17,11 @@ const Login = () => {
           <input type="password" name="password" />
         </div>
         <div>
-          <input
-            className="signupButton"
-            type="submit"
-            value="Log In"
-            onClick={function () {
-              // FIX
-              window.location = "/loggedon"
-            }}
-          />
+          <input className="signupButton" type="submit" value="Log In" />
           <hr />
           <div>
             <label htmlFor="dontHaveAnAccount" id="dontHaveAnAccount">
-              Don't have an account yet?{" "}
+              Don't have an account yet?
               <span className="signupSpan">
                 <Link to="/signup"> Sign up.</Link>
               </span>
@@ -64,40 +32,5 @@ const Login = () => {
     </form>
   )
 }
-
-// const Signup = () => {
-//   const {
-//     // register,
-//     // formState: { errors },
-//     // handleSubmit,
-//     watch
-//   } = useForm({})
-//   const password = useRef({})
-//   password.current = watch("password", "")
-
-//   async function submit() {
-//     let username = document.getElementById("username").value
-//     let password = document.getElementById("password").value
-
-//     console.log("username:", username)
-//     console.log("password:", password)
-
-//     let submissionData = {
-//       username: username,
-//       password: password
-//     }
-
-//     let fetchUrl = "/signup"
-//     let fetchOptions = {
-//       method: "post",
-//       headers: { "content-type": "application/json" },
-//       body: JSON.stringify(submissionData)
-//     }
-
-//     let response = await fetch(fetchUrl, fetchOptions)
-//     let resObject = await response.json()
-//     console.log(resObject)
-//   }
-// }
 
 export default Login
