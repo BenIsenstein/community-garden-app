6694861e27b2a3c35e072827cd17606c9508b599
Ben Isenstein <ben.isenstein@gmail.com>
2021-05-25 18:13:37 -0600

    AddGardenForm now includes the fields we discussed adding, and renders errors. addGarden router also accepts everything.

--- client/src/components/AddGardenForm/AddGardenForm.js
+++ client/src/components/AddGardenForm/AddGardenForm.js
@@ -26,8 +26,7 @@ function AddGardenForm({formCoordinates}) {
             id='Garden-name' 
             name='nameData'
             onChange={onChange}
-            {...register(
-              'nameData', 
+            {...register('nameData', 
               {
                 validate: async (name) => await checkIsNameFree(name) || 'That name is taken.',
                 required: 'You must input a name for your garden.'
@@ -44,12 +43,7 @@ function AddGardenForm({formCoordinates}) {
               id='address' 
               name='addressData'
               onChange={onChange}
-              {...register(
-                'addressData', 
-                {
-                validate: async (address) => await checkIsAddressFree(address) || 'That address is taken'
-                }
-              )}
+              {...register('addressData', {validate: async (address) => await checkIsAddressFree(address) || 'That address is taken'})}
             />
             {errors.addressData && <p>{errors.addressData.message}</p>}
           </div>
@@ -58,11 +52,13 @@ function AddGardenForm({formCoordinates}) {
             <input 
               type='hidden'
               name='lat'
+              onChange={onChange}
               {...register('lat', {validate: lat => lat !== 0 || 'You must choose coordinates.'})}
             />
             <input 
               type='hidden'
               name='lng'
+              onChange={onChange}
               {...register('lng')}
             />
             <div id='coordinates'>
@@ -73,25 +69,38 @@ function AddGardenForm({formCoordinates}) {
           </div>
         </div>
         <div className='Garden-form-element'>
+          <label htmlFor='Established'>Established</label>
+          <input 
+            className='Established' 
+            id='Established' 
+            name='establishedData'
+            type='number'
+            min='1900'
+            {...register('establishedData', {required: 'You must input a year established.'})}
+          />
+          {errors.establishedData && <p>{errors.establishedData.message}</p>}
+        </div>
+        <div className='Garden-form-element'>
           <label htmlFor='quadrant'>Quadrant</label>
           <div className='Quadrant-buttons' id='quadrant'>
             <div className='button'>
               <label htmlFor="NW">NW</label>
-              <input type="radio" id="NW" name="quadrantData" value="NW" {...register('quadrantData', {required: {value: true, message: 'You must select a quadrant.'}})}/>  
+              <input type="radio" id="NW" name="quadrantData" value="NW" {...register('quadrantData', {required: 'You must select a quadrant.'})}/>  
             </div>
             <div className='button'>
               <label htmlFor="NE">NE</label>
-              <input type="radio" id="NE" name="quadrantData" value="NE" {...register('quadrantData', {required: {value: true, message: 'You must select a quadrant.'}})}/>  
+              <input type="radio" id="NE" name="quadrantData" value="NE" {...register('quadrantData', {required: 'You must select a quadrant.'})}/>  
             </div>   
             <div className='button'>
               <label htmlFor="SW">SW</label>
-              <input type="radio" id="SW" name="quadrantData" value="SW" {...register('quadrantData', {required: {value: true, message: 'You must select a quadrant.'}})}/>  
+              <input type="radio" id="SW" name="quadrantData" value="SW" {...register('quadrantData', {required: 'You must select a quadrant.'})}/>  
             </div>
             <div className='button'>
               <label htmlFor="SE">SE</label>
-              <input type="radio" id="SE" name="quadrantData" value="SE" {...register('quadrantData', {required: {value: true, message: 'You must select a quadrant.'}})}/>  
+              <input type="radio" id="SE" name="quadrantData" value="SE" {...register('quadrantData', {required: 'You must select a quadrant.'})}/>  
             </div>   
           </div>
+          {errors.quadrantData && <p>{errors.quadrantData.message}</p>}
         </div>
         <div className='Garden-form-element'>   
           <label className='Cover-photo-label' htmlFor='Cover-photo'>Cover Photo</label>
@@ -104,35 +113,106 @@ function AddGardenForm({formCoordinates}) {
           />
         </div> 
         <div className='Garden-form-element'>
-          <label htmlFor='Surface-area'>Total Area (sqft)</label>
+          <label htmlFor='Plot-size'>Plot Size (sqft)</label>
           <input 
-            className='Surface-area' 
-            id='Surface-area' 
-            name='surfaceAreaData'
+            className='Plot-size' 
+            id='Plot-size' 
+            name='plotSizeData'
             type='number'
             min='0'
-            {...register(
-              'surfaceAreaData', {
-                required: {
-                  value: true, 
-                  message: 'You must input a surface area.'
-                }
-              }
-            )}
+            {...register('plotSizeData', {required: 'You must input a plot size.'})}
           />
-        </div>   
+          {errors.plotSizeData && <p>{errors.plotSizeData.message}</p>}
+        </div>
+        <div className='Garden-form-element'>
+          <label htmlFor='Number-of-plots'>Number of Plots</label>
+          <input 
+            className='Number-of-plots' 
+            id='Number-of-plots' 
+            name='numberOfPlotsData'
+            type='number'
+            min='0'
+            {...register('numberOfPlotsData', {required: 'You must input a number of plots.'})}
+          />
+          {errors.numberOfPlotsData && <p>{errors.numberOfPlotsData.message}</p>}
+        </div>
+        <div className='Garden-form-element'>
+          <label htmlFor='Postal-code'>Postal Code</label>
+          <input 
+            className='Postal-code' 
+            id='Postal-code' 
+            name='postalCodeData'
+            {...register('postalCodeData', {required: 'You must input a postal code.'})}
+          />
+          {errors.postalCodeData && <p>{errors.postalCodeData.message}</p>}
+        </div>      
+        <div className='Garden-form-element'>
+          <label htmlFor='Fee'>Annual Membership Fee</label>
+          <input 
+            className='Fee' 
+            id='Fee' 
+            name='feeData'
+            placeholder='Leave blank if no fee'
+            {...register('feeData')}
+          />
+        </div>
         <div className='Garden-form-element'> 
           <label htmlFor='vacancy'>Vacancy</label>
           <div className='Vacancy-buttons'id='vacancy'>
             <div className='button'>
               <label htmlFor="yes">Yes</label>
-              <input type="radio" id="yes" name="vacancyData" value="yes" {...register('vacancyData', {required: {value: true, message: 'You must declare whether you have room for new gardeners.'}})}/>
+              <input type="radio" id="yes" name="vacancyData" value="yes" {...register('vacancyData', {required:  'You must declare whether you have room for new gardeners.'})}/>
             </div>
             <div className='button'>
               <label htmlFor="no">No</label>
-              <input type="radio" id="no" name="vacancyData" value="no" {...register('vacancyData', {required: {value: true, message: 'You must declare whether you have room for new gardeners.'}})}/>
+              <input type="radio" id="no" name="vacancyData" value="no" {...register('vacancyData', {required: 'You must declare whether you have room for new gardeners.'})}/>
             </div>   
-          </div>  
+          </div> 
+          {errors.vacancyData && <p>{errors.vacancyData.message}</p>} 
+        </div> 
+        <div className='Garden-form-element'>
+          <label htmlFor='Description'>Description</label>
+          <input 
+            className='Description' 
+            id='Description' 
+            name='descriptionData'
+            {...register('descriptionData', {required: "You must write a description."})}
+          />
+          {errors.descriptionData && <p>{errors.descriptionData.message}</p>}
+        </div>  
+        <div className='Garden-form-element'>
+          <label htmlFor='Website'>Website</label>
+          <input 
+            className='Website' 
+            id='Website' 
+            name='websiteData'
+            placeholder='Leave blank if no website'
+            {...register('websiteData')}
+          />
+        </div> 
+        <div className='Garden-form-element'>
+          <label htmlFor='Email'>Garden Admin Email</label>
+          <input 
+            className='Email' 
+            id='Email' 
+            name='emailData'
+            {...register('emailData', {required: "You must include an email for security reasons. We won't spam you, we promise :)"})}
+          />
+          {errors.emailData && <p>{errors.emailData.message}</p>}
+        </div> 
+        <div className='Garden-form-element'> 
+          <label htmlFor='Accessibility'>Is your garden Wheelchair-Accessible?</label>
+          <div className='Accessibility-buttons'id='Accessibility'>
+            <div className='button'>
+              <label htmlFor="yes">Yes</label>
+              <input type="radio" id="yes" name="accessibilityData" value="yes" {...register('accessibilityData', {required:  'You must declare whether your garden is wheelchair-accessible.'})}/>
+            </div>
+            <div className='button'>
+              <label htmlFor="no">No</label>
+              <input type="radio" id="no" name="accessibilityData" value="no" {...register('accessibilityData', {required: 'You must declare whether your garden is wheelchair-accessible.'})}/>
+            </div>   
+          </div> 
+          {errors.accessibilityData && <p>{errors.accessibilityData.message}</p>} 
         </div> 
         <div className='Garden-form-element'>
           <input type='submit' value='submit' />
@@ -149,10 +229,11 @@ function AddGardenForm({formCoordinates}) {
       headers: {'content-type': 'application/json'},
       body: JSON.stringify(submissionData)
     }
-    let response = await fetch(fetchUrl, fetchOptions)
-    let resObject = await response.json()
-  
+    
     try {
+      let response = await fetch(fetchUrl, fetchOptions)
+      let resObject = await response.json()
+
       return resObject.result
     }
     catch (err) {
@@ -169,17 +250,17 @@ function AddGardenForm({formCoordinates}) {
       headers: {'content-type': 'application/json'},
       body: JSON.stringify(submissionData)
     }
-    let response = await fetch(fetchUrl, fetchOptions)
-    let resObject = await response.json()
-  
+
     try {
+      let response = await fetch(fetchUrl, fetchOptions)
+      let resObject = await response.json()
+  
       return !address ? true : resObject.result
     }
     catch (err) {
       console.error('Error validating address in MongoDBAtlas Cluster!', err)
     }
   }
-
 
   async function submitAddGardenForm(data) {
     data.coordinatesData={lat, lng}
@@ -190,15 +271,18 @@ function AddGardenForm({formCoordinates}) {
       headers: {'content-type': 'application/json'},
       body: JSON.stringify(data)
     }
-    let response = await fetch(fetchUrl, fetchOptions)
-    let resObject = await response.json()
 
-    alert(resObject.successMessage)
+    try {
+      let response = await fetch(fetchUrl, fetchOptions)
+      let resObject = await response.json()
+
+      alert(resObject.successMessage)
+    }
+    catch (err) {
+      alert("There was an error adding your garden. We're solving it as fast as we can.")
+      console.error('Error submitting garden to server!', err)
+    }
   }
-}
-
-AddGardenForm.defaultProps = {
-  name: 'AddGardenForm'
 }
 
 export default AddGardenForm
