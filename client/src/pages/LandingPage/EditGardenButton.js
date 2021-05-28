511521bc590bc9d21eaaae5eb93230bc72292de4
Christy <57055910+christyc01@users.noreply.github.com>
2021-05-27 22:57:58 -0600

    Add placeholders for about, messages, and to-do on individual garden page; adjust styling

--- client/src/pages/LandingPage/EditGardenButton.js
+++ client/src/pages/LandingPage/EditGardenButton.js
@@ -1,12 +1,14 @@
 import { useHistory, useParams } from "react-router-dom"
+import { Button } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 export default function EditGardenButton() {
     const { gardenName } = useParams()
     const history = useHistory()
 
     return (
-      <button onClick={() => history.push(`/garden-page/${gardenName}/edit`)}>
+      <Button className="button" primary circular onClick={() => history.push(`/garden-page/${gardenName}/edit`)}>
         Edit this garden
-      </button>
+      </Button>
     )
 }
--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'
 import EditGardenButton from "./EditGardenButton";
+import "../../components/Weather/styles.css"
+import squashes from "../../images/squashes.jpg"
 
 export default function LandingPage() {
   const { gardenName } = useParams()
@@ -24,17 +26,40 @@ export default function LandingPage() {
         {!gardenData && 'Loading...'}
         {(gardenData === 'no garden') && 'This garden could not be found.'}
         {(gardenData !== 'no garden') && gardenData?.name}
-        {(typeof gardenData === 'object') && <EditGardenButton />}
 
       </h1>
       <div>
         {gardenData?.address}
       </div>
 
-      <div class ="header2"><h2>History</h2>
-     
+      <div class="round-background">
+        <h2 class="section-heading">About Our Garden</h2>
+        <div class="about-our-garden">
+          <div class="about-our-garden-text">
+            <p>Reader Rock Garden Historic Park is one of Calgary's most unique cultural landscapes featuring the restored Reader house (that contains Reader's Garden Café), rock pathways, bridges, benches and beautiful flowers. Reader Rock Garden is the perfect location for group functions and events, including weddings, photography sessions and family gatherings. Book Reader Rock Garden for your function.</p>
+          </div>
+          <div class="about-our-garden-text">
+            <img src={squashes} alt="" className="about-our-garden-photo"/>
+          </div>
+        </div>
       </div>
 
+      <div class="messages-and-to-do">
+        <div class="round-background message-board">
+          <h2 class="section-heading ">Message Board</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt similique sapiente aspernatur. Magni quaerat voluptas nisi commodi dolor saepe reiciendis.</p>
+        </div>
+        <div class="round-background to-do-list">
+          <h2 class="section-heading ">To-Do List</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt similique sapiente aspernatur. Magni quaerat voluptas nisi commodi dolor saepe reiciendis.</p>
+        </div>
+      </div>
+
+    <footer>
+      <div>
+          {(typeof gardenData === 'object') && <EditGardenButton />}
+      </div>
+    </footer>
 
     </div>
 
