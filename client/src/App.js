8dc813f73e4dccd431541606cb46c79f4793eb4a
Brady Walker-Brock <bwalkerbr@gmail.com>
2021-05-29 13:52:26 -0600

    updated imports

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,6 @@ import Login from "./components/User/Login"
 import LoggedOn from "./components/User/LoggedOn"
 import GetWeather from "./components/Weather/GetWeather"
 import GetForecast from "./components/Weather/GetForecast"
-import GardenMap from "./components/gardenMap/GardenMap"
-import GardenTable from "./components/DataTable/GardenTable"
 import GardenPageRouter from "./components/IndividualGardens/GardenPageRouter"
 import AuthenticationProvider from "./AuthenticationProvider"
 import LogInOrOut from "./components/User/LogInOrOut"
@@ -75,18 +73,12 @@ function App() {
             <Route path="/loggedon">
               <LoggedOn />
             </Route>
-            <Route path="/gardenMap">
-              <GardenMap />
-            </Route>
             <Route path="/weather">
               <GetWeather />
             </Route>
             <Route path="/forecast">
               <GetWeather />
               <GetForecast />
-            </Route>
-            <Route path="/table">
-              <GardenTable />
             </Route>
             <Route path="/garden-page">
               <GardenPageRouter />
