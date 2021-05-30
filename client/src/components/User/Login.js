cc63fa9e5c6a2aeb5f40e992038dd6e169ac0b2a
Christy <57055910+christyc01@users.noreply.github.com>
2021-05-30 10:15:53 -0600

    Tidy up styling for login/signup forms

--- client/src/components/User/Login.js
+++ client/src/components/User/Login.js
@@ -9,41 +9,43 @@ const Login = () => {
   const { register, formState: { errors }, handleSubmit } = useForm({})
 
   return (
-    <form className="signupForm" onSubmit={handleSubmit(async (data) => await authContext.logIn(data))}>
-      <div className="container">
-        <h1>Log In!</h1>
-        <div className="form-control">
-          <label htmlFor="username">Username:</label>
-          <input 
-            type="text" 
-            name="username" 
-            {...register("username", {required: "You must input a username."})}
-          />
-          {errors.username && <p>{errors.username.message}</p>}
-        </div>
-        <div className="form-control">
-          <label htmlFor="password">Password:</label>
-          <input 
-            type="password" 
-            name="password" 
-            {...register("password", {required: "You must input a password."})}
-          />
-          {errors.password && <p>{errors.password.message}</p>}
-        </div>
-        <div>
-          <input className="signupButton" type="submit" value="Log In" />
-          <hr />
+    <div className="loginBackground">
+      <form className="signupForm" onSubmit={handleSubmit(async (data) => await authContext.logIn(data))}>
+        <div className="container">
+          <h1>Log In!</h1>
+          <div className="form-control">
+            <label htmlFor="username">Username:</label>
+            <input 
+              type="text" 
+              name="username" 
+              {...register("username", {required: "You must input a username."})}
+            />
+            {errors.username && <p>{errors.username.message}</p>}
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password:</label>
+            <input 
+              type="password" 
+              name="password" 
+              {...register("password", {required: "You must input a password."})}
+            />
+            {errors.password && <p>{errors.password.message}</p>}
+          </div>
           <div>
-            <label htmlFor="dontHaveAnAccount" id="dontHaveAnAccount">
-              Don't have an account yet?
-              <span className="signupSpan">
-                <Link to="/signup"> Sign up</Link>
-              </span>
-            </label>
+            <input className="signupButton" type="submit" value="Log In" />
+            <hr />
+            <div>
+              <label htmlFor="dontHaveAnAccount" id="dontHaveAnAccount">
+                Don't have an account yet?
+                <span className="signupSpan">
+                  <Link to="/signup"> Sign up</Link>
+                </span>
+              </label>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   )
 }
 
