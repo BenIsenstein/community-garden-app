53d5b7bc0ff0f031a933a1b07f98658e2c69684c
Christy <57055910+christyc01@users.noreply.github.com>
2021-05-30 23:28:01 -0600

    Adjust formatting to make background colour appear on full page; adjust team photo card formatting

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,3 +1,4 @@
+import "../../../src/App.css"
 import "./about.css"
 import Brady from "../../components/images/brady.jpg"
 import Christy from "../../components/images/Christy.jpg"
@@ -5,71 +6,113 @@ import donna from "../../components/images/donna.jpg"
 import Ben from "../../components/images/Ben.jpg"
 
 export default function About() {
+  return (
+    <div className="pageBackground">
+      <div>
+        <div className="about-section">
+          <h1>About Us Page</h1>
+          <p>Some text about who we are and what we do.</p>
+          <p>Resize the browser window to see that this page is responsive by the way.</p>
+        </div>
 
- return (
-
-<div>    
-    <div className="about-section">
-        <h1>About Us Page</h1>
-        <p>Some text about who we are and what we do.</p>
-        <p>Resize the browser window to see that this page is responsive by the way.</p>
-    </div>
-    
-    <h2>Our Team</h2>
-    <div class="row">
-        <div class="column">
+        <h2 className="about-heading">Our Team</h2>
+        <div class="row">
+          <div class="column">
             <div class="card">
-            <div className="team-image" style={{height: "275px", backgroundImage: "url(" + Ben + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}/>
-            {/* <img src={Ben} alt="Ben" width="400" height="275"/> */}
-            <div class="container">
+              <div
+                className="team-image"
+                style={{
+                  height: "275px",
+                  backgroundImage: "url(" + Ben + ")",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat"
+                }}
+              />
+              <div class="container">
                 <h2>Ben</h2>
                 <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                 <p>ben@example.com</p>
-                <p><button className="About-button">Contact</button></p>
+                <p>
+                  <button className="About-button">Contact</button>
+                </p>
+              </div>
             </div>
-        </div>
-    </div>
+          </div>
 
-    <div class="column">
-        <div class="card">
-            <div className="team-image" style={{height: "275px", backgroundImage: "url(" + Brady + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}/>
-            {/* <img src={Brady} alt="Brady" width="200" height="275" /> */}
-            <div class="container">
+          <div class="column">
+            <div class="card">
+              <div
+                className="team-image"
+                style={{
+                  height: "275px",
+                  backgroundImage: "url(" + Brady + ")",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat"
+                }}
+              />
+              <div class="container">
                 <h2>Brady</h2>
                 <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                 <p>brady@example.com</p>
-                <p><button className="About-button">Contact</button></p>
+                <p>
+                  <button className="About-button">Contact</button>
+                </p>
+              </div>
             </div>
-        </div>
-    </div>
+          </div>
 
-
-    <div class="column">
-        <div class="card">
-            <div className="team-image" style={{height: "275px", backgroundImage: "url(" + Christy + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}/>
-            <div class="container">
+          <div class="column">
+            <div class="card">
+              <div
+                className="team-image"
+                style={{
+                  height: "275px",
+                  backgroundImage: "url(" + Christy + ")",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat"
+                }}
+              />
+              <div class="container">
                 <h2>Christy</h2>
                 <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                 <p>christy@example.com</p>
-                <p><button className="About-button">Contact</button></p>
+                <p>
+                  <button className="About-button">Contact</button>
+                </p>
+              </div>
             </div>
-        </div>
-    </div>
+          </div>
 
-    <div class="column">
-        <div class="card">
-        <div className="team-image" style={{height: "275px", backgroundImage: "url(" + donna + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}/>
-            {/* <img src={donna} alt="Donna" width="200" height="275" /> */}
-            <div class="container">
+          <div class="column">
+            <div class="card">
+              <div
+                className="team-image"
+                style={{
+                  height: "275px",
+                  backgroundImage: "url(" + donna + ")",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat"
+                }}
+              />
+              <div class="container">
                 <h2>Donna</h2>
-                <p>Beans, peas and pattypan squash are among my most succesful garden crops. Growing my full-stack development skill-set.</p>
+                <p>
+                  Beans, peas and pattypan squash are among my most succesful garden crops. Growing
+                  my full-stack development skill-set.
+                </p>
                 <p>donna@example.com</p>
-                <p><button className="About-button">Contact</button></p>
+                <p>
+                  <button className="About-button">Contact</button>
+                </p>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-</div>
- )
- }
-
+  )
+}
