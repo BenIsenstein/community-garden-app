d210b1bc28294799ad820cd252a26379e486ce6e
Christy <57055910+christyc01@users.noreply.github.com>
2021-06-01 18:51:42 -0600

    Add team/problem/solution/technologies sections to About page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -15,103 +15,130 @@ export default function About() {
           <p>Resize the browser window to see that this page is responsive by the way.</p>
         </div>
 
-        <h2 className="about-heading">Our Team</h2>
-        <div className="row">
-          <div className="column">
-            <div className="card">
-              <div
-                className="team-image"
-                style={{
-                  height: "275px",
-                  backgroundImage: "url(" + Ben + ")",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-                }}
-              />
-              <div className="container">
-                <h2>Ben</h2>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>ben@example.com</p>
-                <p>
-                  <button className="About-button">Contact</button>
-                </p>
+        <div className="about-subsection">
+          <h2 className="about-heading">The Team</h2>
+          <div className="row">
+            <div className="column">
+              <div className="card">
+                <div
+                  className="team-image"
+                  style={{
+                    height: "275px",
+                    backgroundImage: "url(" + Ben + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                  }}
+                />
+                <div className="container">
+                  <h2>Ben</h2>
+                  <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                  <p>ben@example.com</p>
+                  <p>
+                    <button className="About-button">Contact</button>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="column">
-            <div className="card">
-              <div
-                className="team-image"
-                style={{
-                  height: "275px",
-                  backgroundImage: "url(" + Brady + ")",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-                }}
-              />
-              <div className="container">
-                <h2>Brady</h2>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>brady@example.com</p>
-                <p>
-                  <button className="About-button">Contact</button>
-                </p>
+            <div className="column">
+              <div className="card">
+                <div
+                  className="team-image"
+                  style={{
+                    height: "275px",
+                    backgroundImage: "url(" + Brady + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                  }}
+                />
+                <div className="container">
+                  <h2>Brady</h2>
+                  <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                  <p>brady@example.com</p>
+                  <p>
+                    <button className="About-button">Contact</button>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="column">
-            <div className="card">
-              <div
-                className="team-image"
-                style={{
-                  height: "275px",
-                  backgroundImage: "url(" + Christy + ")",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-                }}
-              />
-              <div className="container">
-                <h2>Christy</h2>
-                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                <p>christy@example.com</p>
-                <p>
-                  <button className="About-button">Contact</button>
-                </p>
+            <div className="column">
+              <div className="card">
+                <div
+                  className="team-image"
+                  style={{
+                    height: "275px",
+                    backgroundImage: "url(" + Christy + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                  }}
+                />
+                <div className="container">
+                  <h2>Christy</h2>
+                  <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                  <p>christy@example.com</p>
+                  <p>
+                    <button className="About-button">Contact</button>
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="column">
-            <div className="card">
-              <div
-                className="team-image"
-                style={{
-                  height: "275px",
-                  backgroundImage: "url(" + donna + ")",
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat"
-                }}
-              />
-              <div className="container">
-                <h2>Donna</h2>
-                <p>
-                  Beans, peas and pattypan squash are among my most succesful garden crops. Growing
-                  my full-stack development skill-set.
-                </p>
-                <p>donna@example.com</p>
-                <p>
-                  <button className="About-button">Contact</button>
-                </p>
+            <div className="column">
+              <div className="card">
+                <div
+                  className="team-image"
+                  style={{
+                    height: "275px",
+                    backgroundImage: "url(" + donna + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                  }}
+                />
+                <div className="container">
+                  <h2>Donna</h2>
+                  <p>
+                    Beans, peas and pattypan squash are among my most succesful garden crops. Growing
+                    my full-stack development skill-set.
+                  </p>
+                  <p>donna@example.com</p>
+                  <p>
+                    <button className="About-button">Contact</button>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="about-subsection">
+          <h2 className="about-heading">The Problem</h2>
+          <div className="row">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus, maxime adipisci iure itaque tenetur quam quas doloremque officia omnis enim sapiente animi est expedita. Ex molestiae at distinctio deserunt.
+            </p>
+          </div>
+        </div>
+        <div className="about-subsection">
+          <h2 className="about-heading">The Solution</h2>
+          <div className="row">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus, maxime adipisci iure itaque tenetur quam quas doloremque officia omnis enim sapiente animi est expedita. Ex molestiae at distinctio deserunt.
+            </p>
+          </div>
+        </div>
+        <div className="about-subsection">
+          <h2 className="about-heading">The Technologies</h2>
+          <div className="row">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate ducimus, maxime adipisci iure itaque tenetur quam quas doloremque officia omnis enim sapiente animi est expedita. Ex molestiae at distinctio deserunt.
+            </p>
+          </div>
+        </div>
+
       </div>
     </div>
   )
